891a247ae5eee50f9ce4d58ca09e7981c3963680
WillP-KEEN <William@keenpayroll.ca>
2024-11-20 16:21:05 -0330

    cameron is the goat

--- PDFTest.py
+++ PDFTest.py
@@ -9,34 +9,20 @@ template = env.get_template("template.html")
 
 html_out = template.render()
 
-wkhtmltopdfRelPath = "wkhtmltopdfQT/bin/wkhtmltopdf"
-wkhtmltopdfAbsPath = os.path.abspath(wkhtmltopdfRelPath)
-
-st.write(wkhtmltopdfAbsPath)
-st.write(f"Permissions for wkhtmltopdf:{oct(os.stat(wkhtmltopdfAbsPath).st_mode)}")
-
-
-
-
-wkhtml_path = pdfkit.configuration(wkhtmltopdf = wkhtmltopdfAbsPath)
 with open("output.html", "w") as f:
     f.write(html_out)
 
 options = {
         "enable-local-file-access": "",  # Enabling local file access
-        "header-html": "header.html",  # Path to header HTML file
-        "footer-html": "footer.html",  # Path to footer HTML file
-        "margin-top": "30mm",  # Set margin to accommodate header
+        "margin-top": "0mm",  # Set margin to accommodate header
         "margin-left": "0mm",
         "margin-right": "0mm",
-        "margin-bottom": "25mm",  # Set margin to accommodate footer
+        "margin-bottom": "0mm",  # Set margin to accommodate footer
         "page-size": "Letter",
     }
 
-st.write(wkhtml_path)
 
-
-pdfkit.from_file("output.html", "output.pdf", options=options, configuration=wkhtml_path)
+pdfkit.from_file("output.html", "output.pdf", options=options)
 
 with open("output.pdf", "rb") as pdf_file:
         PDFbyte = pdf_file.read()
